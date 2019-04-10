9d7d03103b9e3c53c569c0daa660ad32c6b4f4e7
lduttonucsd <duttonaudioworks@gmail.com>
2019-04-10 11:05:24 -0700

    added SQL teams DB data for OWL and CSGO

---
+++ db/teamsOWL.sql
@@ -0,0 +1,217 @@
+USE teamsDB;
+
+CREATE TABLE OWLplayers (
+id INT AUTO_INCREMENT,
+team VARCHAR (255),
+name VARCHAR (255),
+role VARCHAR(255),
+PRIMARY KEY (id)
+);
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Erster', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'frd', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'NLaaer', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Dogman', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Gator', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Kodak', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Daco', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'Pokpo', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'masaa', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('ATLreign', 'babybay', 'DPS');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'Persia', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'Axxiom', 'tank'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'Kellex', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'Fusions', 'tank'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'RCK', 'tank'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'blase', 'DPS'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'Aimgod', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'ColourHex', 'DPS'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('BSTuprising', 'alemao', 'support'); 
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Yveltal', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Elsa', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'GARRY', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Baconjack', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Kyo', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Ameng', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'lateyoung', 'tank');     
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'jiqiren', 'tank'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'TangXiaoLong', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('CHEhunters', 'Jinmu', 'DPS');  
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'Harryhook', 'support'); 
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'uNKOE', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'Taimou', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'Mickie', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'NotE', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'AKM', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'OGE', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'Closer', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'ZachaREEE', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('DALfuel', 'EFFECT', 'DPS');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'HaGoPeun', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'sayaplayer', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'xepheR', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'Apply', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'SNT', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'SWoN', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'kris', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'bqb', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'McGravy', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('FLOmayhem', 'Tviq', 'DPS');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Rise', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Rio', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Happy', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Onlywish', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Eileen', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Shu', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'nero', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Kyb', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'HOTBA', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('GANcharge', 'Chara', 'support');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Krystal', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'BEBE', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Adora', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Guxue', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Revenge', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'GodsB', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'iDK', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Bazzi', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'SASIN', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'Ria', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HANspark', 'NoSmite', 'tank');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Muma', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Boink', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'SPREE', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Rawkus', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Danteh', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Bani', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'LiNkzer', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'ArHan', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'Jake', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('HOUoutlaws', 'coolmatt', 'tank');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Gesture', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Bdosin', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Guard', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Profit', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'BIRDRING', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'NUS', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Fury', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LDNspitfire', 'Krillin', 'support');
+
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'r0ar', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'shaz', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'BigG00se', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Surefour', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Ripa', 'support');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Bischu', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Hydration', 'DPS');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Panker', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Void', 'tank');
+INSERT INTO OWLplayers (team, name, role)
+VALUES ('LAgladiators', 'Decay', 'DPS');
+
+SELECT * FROM OWLplayers;
